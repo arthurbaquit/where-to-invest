46af2a3a2a9837433950f0ccb0c3fadd5058275e
Arthur <arthur.baquit@hotmail.com>
2023-05-29 02:14:49 +0900

    add: onRemove

--- client/src/components/AtivoCard.tsx
+++ client/src/components/AtivoCard.tsx
@@ -1,16 +1,22 @@
 import { AtivosWithInvestAmount } from "../helpers/GetWhereToInvest";
-import { Card } from "./UI/styles";
+import { Button, Card, DangerButton } from "./UI/styles";
 
 type AtivoCardProps = {
   ativo: AtivosWithInvestAmount;
+  onRemove: (ativo: AtivosWithInvestAmount) => void;
 };
-export const AtivoCard = ({ ativo }: AtivoCardProps) => {
+export const AtivoCard = ({ ativo, onRemove }: AtivoCardProps) => {
+  const onHandleClick = () => {
+    onRemove(ativo);
+  };
+
   return (
-    <Card>
+    <Card style={{ display: "flex", justifyContent: "space-between" }}>
       <p>
         Ativo: {ativo.nome} - Posição: {ativo.posicao} - Meta: {ativo.meta}% -
         Tipo: {ativo.tipo} - Aporte: {ativo.investAmount.toFixed(2)}
       </p>
+      <DangerButton onClick={onHandleClick}> Apagar ativo</DangerButton>
     </Card>
   );
 };
