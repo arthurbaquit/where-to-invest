b3400f79cea33271684919588d5490a98b997359
Arthur <arthur.baquit@hotmail.com>
2023-08-20 15:06:13 -0300

    rm: commented code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Ativos } from "./components/Ativos";
 import { DoughnutChart } from "./components/UI/DoughnutChart";
@@ -7,14 +5,6 @@ import { DoughnutChart } from "./components/UI/DoughnutChart";
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <DoughnutChart />
       <h1>Onde Investir</h1>
       <p>
@@ -33,17 +23,6 @@ function App() {
         <li> Clique em calcular e bons investimentos!</li>
       </ul>
       <Ativos />
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
